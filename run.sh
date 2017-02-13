716411d3610a976458da6bbf8a8194a7598a8d44
Ian Graham <imartingraham@gmail.com>
2017-02-13 12:47:05 -0800

    change awsebcli version

--- run.sh
+++ run.sh
@@ -50,7 +50,7 @@ echo 'Installing pip...'
 sudo apt-get install -y python-pip libpython-all-dev
 
 echo 'Installing awscli...'
-sudo pip install awsebcli==3.8.8
+sudo pip install awsebcli==3.7.4
 
 echo 'eb version show...'
 eb --version
